baa94d6ed60a2589eee0e0c38d95615956f2e292
Natasha Craike <ncraike@gmail.com>
2023-07-18 22:17:33 +1000

    Adds colors.rs with constants for each colour

---
+++ src/ui/colors.rs
@@ -0,0 +1,30 @@
+use bracket_color::prelude::RGB;
+
+pub const GREEN_DARK: RGB = RGB::from_u8(41, 66, 57);
+pub const GREEN_MID: RGB = RGB::from_u8(107, 148, 82);
+pub const GREEN_LIGHT: RGB = RGB::from_u8(156, 173, 132);
+
+pub const BLUE_DARK: RGB = RGB::from_u8(28, 33, 38);
+pub const BLUE_MID_DARK: RGB = RGB::from_u8(74, 115, 148);
+pub const BLUE_MID_LIGHT: RGB = RGB::from_u8(66, 148, 173);
+pub const BLUE_LIGHT: RGB = RGB::from_u8(156, 189, 181);
+
+pub const TEAL_DARK: RGB = RGB::from_u8(41, 68, 77);
+pub const TEAL_MID: RGB = RGB::from_u8(49, 123, 123);
+pub const TEAL_LIGHT: RGB = RGB::from_u8(107, 148, 148);
+
+pub const BROWN_DARK: RGB = RGB::from_u8(27, 24, 25);
+pub const BROWN_MID_DARK: RGB = RGB::from_u8(52, 46, 45);
+pub const BROWN_MID_MID: RGB = RGB::from_u8(74, 66, 57);
+pub const BROWN_MID_LIGHT: RGB = RGB::from_u8(123, 90, 49);
+pub const BROWN_LIGHT: RGB = RGB::from_u8(247, 198, 165);
+
+pub const BEIGE_DARK: RGB = RGB::from_u8(121, 95, 70);
+pub const BEIGE_MID_DARK: RGB = RGB::from_u8(175, 123, 78);
+pub const BEIGE_MID_LIGHT: RGB = RGB::from_u8(219, 159, 112);
+pub const BEIGE_LIGHT: RGB = RGB::from_u8(173, 165, 165);
+
+pub const BRIGHT_MAGENTA: RGB = RGB::from_u8(206, 66, 107);
+pub const BRIGHT_RED: RGB = RGB::from_u8(230, 42, 42);
+pub const BRIGHT_ORANGE: RGB = RGB::from_u8(231, 99, 82);
+pub const BRIGHT_YELLOW: RGB = RGB::from_u8(231, 188, 82);
