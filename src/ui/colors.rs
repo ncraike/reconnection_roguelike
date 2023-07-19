f310d080f1516b3b82f82d5a23454ed17bd400c5
Natasha Craike <ncraike@gmail.com>
2023-07-19 23:33:12 +1000

    Re-adds colors.rs with actual constant values
    Rewrote ui::colors to actually define values in a way which `const`
    accepts: instead of calling `RGB::from_u8`, we now define the `RGB`
    structs directly.
    
    Unfortunately this means we're again specifying floats from 0.0 to 1.0,
    which means we're again defining values divided by `255.0`.
    
    Also added some `RGBA` values for complete transparency, opaque white
    and opaque grey. These are mostly used for background colours and for
    shading seen-but-not-visible graphical tiles (see `camera.rs`).

--- src/ui/colors.rs
+++ src/ui/colors.rs
@@ -1,30 +1,141 @@
-use bracket_color::prelude::RGB;
+use bracket_color::prelude::{RGB, RGBA};
 
-pub const GREEN_DARK: RGB = RGB::from_u8(41, 66, 57);
-pub const GREEN_MID: RGB = RGB::from_u8(107, 148, 82);
-pub const GREEN_LIGHT: RGB = RGB::from_u8(156, 173, 132);
+pub const GREEN_DARK: RGB = RGB {
+    r: 41.0 / 255.0,
+    g: 66.0 / 255.0,
+    b: 57.0 / 255.0,
+};
+pub const GREEN_MID: RGB = RGB {
+    r: 107.0 / 255.0,
+    g: 148.0 / 255.0,
+    b: 82.0 / 255.0,
+};
+pub const GREEN_LIGHT: RGB = RGB {
+    r: 156.0 / 255.0,
+    g: 173.0 / 255.0,
+    b: 132.0 / 255.0,
+};
 
-pub const BLUE_DARK: RGB = RGB::from_u8(28, 33, 38);
-pub const BLUE_MID_DARK: RGB = RGB::from_u8(74, 115, 148);
-pub const BLUE_MID_LIGHT: RGB = RGB::from_u8(66, 148, 173);
-pub const BLUE_LIGHT: RGB = RGB::from_u8(156, 189, 181);
+pub const BLUE_DARK: RGB = RGB {
+    r: 28.0 / 255.0,
+    g: 33.0 / 255.0,
+    b: 38.0 / 255.0,
+};
+pub const BLUE_MID_DARK: RGB = RGB {
+    r: 74.0 / 255.0,
+    g: 115.0 / 255.0,
+    b: 148.0 / 255.0,
+};
+pub const BLUE_MID_LIGHT: RGB = RGB {
+    r: 66.0 / 255.0,
+    g: 148.0 / 255.0,
+    b: 173.0 / 255.0,
+};
+pub const BLUE_LIGHT: RGB = RGB {
+    r: 156.0 / 255.0,
+    g: 189.0 / 255.0,
+    b: 181.0 / 255.0,
+};
 
-pub const TEAL_DARK: RGB = RGB::from_u8(41, 68, 77);
-pub const TEAL_MID: RGB = RGB::from_u8(49, 123, 123);
-pub const TEAL_LIGHT: RGB = RGB::from_u8(107, 148, 148);
+pub const TEAL_DARK: RGB = RGB {
+    r: 41.0 / 255.0,
+    g: 68.0 / 255.0,
+    b: 77.0 / 255.0,
+};
+pub const TEAL_MID: RGB = RGB {
+    r: 49.0 / 255.0,
+    g: 123.0 / 255.0,
+    b: 123.0 / 255.0,
+};
+pub const TEAL_LIGHT: RGB = RGB {
+    r: 107.0 / 255.0,
+    g: 148.0 / 255.0,
+    b: 148.0 / 255.0,
+};
 
-pub const BROWN_DARK: RGB = RGB::from_u8(27, 24, 25);
-pub const BROWN_MID_DARK: RGB = RGB::from_u8(52, 46, 45);
-pub const BROWN_MID_MID: RGB = RGB::from_u8(74, 66, 57);
-pub const BROWN_MID_LIGHT: RGB = RGB::from_u8(123, 90, 49);
-pub const BROWN_LIGHT: RGB = RGB::from_u8(247, 198, 165);
+pub const BROWN_DARK: RGB = RGB {
+    r: 27.0 / 255.0,
+    g: 24.0 / 255.0,
+    b: 25.0 / 255.0,
+};
+pub const BROWN_MID_DARK: RGB = RGB {
+    r: 52.0 / 255.0,
+    g: 46.0 / 255.0,
+    b: 45.0 / 255.0,
+};
+pub const BROWN_MID_MID: RGB = RGB {
+    r: 74.0 / 255.0,
+    g: 66.0 / 255.0,
+    b: 57.0 / 255.0,
+};
+pub const BROWN_MID_LIGHT: RGB = RGB {
+    r: 123.0 / 255.0,
+    g: 90.0 / 255.0,
+    b: 49.0 / 255.0,
+};
+pub const BROWN_LIGHT: RGB = RGB {
+    r: 247.0 / 255.0,
+    g: 198.0 / 255.0,
+    b: 165.0 / 255.0,
+};
 
-pub const BEIGE_DARK: RGB = RGB::from_u8(121, 95, 70);
-pub const BEIGE_MID_DARK: RGB = RGB::from_u8(175, 123, 78);
-pub const BEIGE_MID_LIGHT: RGB = RGB::from_u8(219, 159, 112);
-pub const BEIGE_LIGHT: RGB = RGB::from_u8(173, 165, 165);
+pub const BEIGE_DARK: RGB = RGB {
+    r: 121.0 / 255.0,
+    g: 95.0 / 255.0,
+    b: 70.0 / 255.0,
+};
+pub const BEIGE_MID_DARK: RGB = RGB {
+    r: 175.0 / 255.0,
+    g: 123.0 / 255.0,
+    b: 78.0 / 255.0,
+};
+pub const BEIGE_MID_LIGHT: RGB = RGB {
+    r: 219.0 / 255.0,
+    g: 159.0 / 255.0,
+    b: 112.0 / 255.0,
+};
+pub const BEIGE_LIGHT: RGB = RGB {
+    r: 173.0 / 255.0,
+    g: 165.0 / 255.0,
+    b: 165.0 / 255.0,
+};
 
-pub const BRIGHT_MAGENTA: RGB = RGB::from_u8(206, 66, 107);
-pub const BRIGHT_RED: RGB = RGB::from_u8(230, 42, 42);
-pub const BRIGHT_ORANGE: RGB = RGB::from_u8(231, 99, 82);
-pub const BRIGHT_YELLOW: RGB = RGB::from_u8(231, 188, 82);
+pub const BRIGHT_MAGENTA: RGB = RGB {
+    r: 206.0 / 255.0,
+    g: 66.0 / 255.0,
+    b: 107.0 / 255.0,
+};
+pub const BRIGHT_RED: RGB = RGB {
+    r: 230.0 / 255.0,
+    g: 42.0 / 255.0,
+    b: 42.0 / 255.0,
+};
+pub const BRIGHT_ORANGE: RGB = RGB {
+    r: 231.0 / 255.0,
+    g: 99.0 / 255.0,
+    b: 82.0 / 255.0,
+};
+pub const BRIGHT_YELLOW: RGB = RGB {
+    r: 231.0 / 255.0,
+    g: 188.0 / 255.0,
+    b: 82.0 / 255.0,
+};
+
+pub const TRANSPARENT: RGBA = RGBA {
+    r: 0.0,
+    g: 0.0,
+    b: 0.0,
+    a: 0.0,
+};
+pub const OPAQUE_WHITE: RGBA = RGBA {
+    r: 1.0,
+    g: 1.0,
+    b: 1.0,
+    a: 1.0,
+};
+pub const OPAQUE_GRAY: RGBA = RGBA {
+    r: 0.7,
+    g: 0.7,
+    b: 0.7,
+    a: 1.0,
+};
