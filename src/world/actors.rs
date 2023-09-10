15c19d293c0c3ad57192ea85d791645cb3834855
Natasha Craike <ncraike@gmail.com>
2023-09-10 15:53:42 +1000

    WIP

--- src/world/actors.rs
+++ src/world/actors.rs
@@ -1,9 +1,20 @@
 use super::types::WorldDirection;
 
+const STANDARD_ACTION_RECOVERY: u64 = 60;
+
 #[derive(PartialEq, Eq, Hash, Clone, Copy, Debug)]
 pub enum Action {
     Move(WorldDirection),
     MeleeAttack(WorldDirection),
     Pickup,
     Wait,
+}
+
+pub fn get_action_recovery(action: Action) -> u64 {
+    match action {
+        Action::Move(_) => STANDARD_ACTION_RECOVERY,
+        Action::MeleeAttack(_) => STANDARD_ACTION_RECOVERY,
+        Action::Pickup => STANDARD_ACTION_RECOVERY,
+        Action::Wait => STANDARD_ACTION_RECOVERY / 12,
+    }
 }
