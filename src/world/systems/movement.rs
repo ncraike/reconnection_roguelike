567d55e77b1d605806d87832189542c6e81df6f8
Natasha Craike <ncraike@gmail.com>
2023-09-17 18:30:00 +1000

    Fix bug: enemies moving into each other

--- src/world/systems/movement.rs
+++ src/world/systems/movement.rs
@@ -1,55 +1,59 @@
 use bracket_geometry::prelude::Point;
+use bracket_terminal::prelude::console;
 use specs::prelude::*;
 
-use super::super::super::components::{Viewshed, WantsToMove};
+use super::super::super::components::{Name, Viewshed, WantsToMove};
 use super::super::super::map::Map;
-use super::super::super::message_log::MessageLog;
 
 pub struct MovementSystem {}
 
 impl<'a> System<'a> for MovementSystem {
     type SystemData = (
         WriteExpect<'a, Map>,
-        WriteExpect<'a, MessageLog>,
+        ReadStorage<'a, Name>,
         WriteStorage<'a, Point>,
         WriteStorage<'a, WantsToMove>,
         WriteStorage<'a, Viewshed>,
     );
 
     fn run(&mut self, data: Self::SystemData) {
-        let (mut map, mut log, mut position_store, mut wants_to_move_store, mut viewshed_store) =
+        let (mut map, name_store, mut position_store, mut wants_to_move_store, mut viewshed_store) =
             data;
 
-        for (wants_to_move, mut position, mut viewshed) in (
+        for (name_component, wants_to_move, mut position, mut viewshed) in (
+            &name_store,
             &wants_to_move_store,
             &mut position_store,
             &mut viewshed_store,
         )
             .join()
         {
-            if !map.bounds().point_in_rect(wants_to_move.destination) {
-                log.entries.push("Could not move out of bounds".to_string());
+            let dest = wants_to_move.destination.clone();
+            let dest_idx = map.to_index(dest);
+            if !map.bounds().point_in_rect(dest) {
+                console::log(format!("{} tried to move out of map", name_component.name));
                 continue;
             }
-            let dest_idx = map.to_index(wants_to_move.destination);
             if map.blocked[dest_idx] {
-                log.entries
-                    .push("Could not move; destination blocked".to_string());
+                // FIXME: improve monster turns to avoid collisions
+                console::log(format!(
+                    "{} could not move to x:{} y:{}, destination blocked",
+                    dest.x, dest.y, name_component.name
+                ));
                 continue;
             }
-
             // Move is possible
 
-            let old_position = wants_to_move.destination.clone();
+            // Copy old position
+            let old_position = position.clone();
             let old_position_idx = map.to_index(old_position);
-
             // Update position
-            position.x = wants_to_move.destination.x;
-            position.y = wants_to_move.destination.y;
+            position.x = dest.x;
+            position.y = dest.y;
             // Update blocking on map
             map.blocked[dest_idx] = true;
             map.blocked[old_position_idx] = false;
-            // Mark entity's viewshed as needing update
+            // Mark entity's vision as needing update
             viewshed.dirty = true;
         }
 
