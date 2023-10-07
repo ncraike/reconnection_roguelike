822535e9554f6d4b1d9cb7533a1b5766b2dd8eb6
Natasha Craike <ncraike@gmail.com>
2023-10-07 17:46:14 +1000

    Restructure into main and lib

---
+++ src/lib.rs
@@ -0,0 +1,72 @@
+pub mod components;
+pub mod map;
+pub mod message_log;
+pub mod types;
+pub mod ui;
+pub mod world;
+
+#[cfg(test)]
+mod tests;
+
+use bracket_lib::prelude::GameState;
+use bracket_terminal;
+use bracket_terminal::prelude::BTerm;
+
+use specs::prelude::*;
+
+use types::{RunState, UITask};
+use ui::UI;
+use world::systems;
+use world::WorldEngine; // FIXME: move to WorldEngine setup
+
+pub const GAME_TITLE: &str = "Reconnection";
+
+pub struct State {
+    pub ecs: World,
+}
+
+// Implement the game loop
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        let ui = *self.ecs.fetch::<UI>();
+        let world_engine = *self.ecs.fetch::<WorldEngine>();
+
+        let mut new_run_state;
+        {
+            let run_state = self.ecs.fetch::<RunState>();
+            new_run_state = *run_state;
+        }
+
+        match new_run_state {
+            RunState::PreRun => {
+                self.run_systems();
+                new_run_state = RunState::DeferToUIFor(UITask::GetPlayerAction);
+            }
+            RunState::DeferToUIFor(ui_task) => match ui_task {
+                UITask::GetPlayerAction => {
+                    new_run_state = ui.defer_to_get_player_action(ctx, &mut self.ecs);
+                }
+                UITask::ShowWorldEvent => {
+                    new_run_state = ui.defer_to_show_world_event(ctx, &mut self.ecs);
+                }
+            },
+            RunState::WorldTick => {
+                new_run_state = world_engine.defer_to(&mut self.ecs);
+            }
+        }
+
+        {
+            let mut run_state_writer = self.ecs.write_resource::<RunState>();
+            *run_state_writer = new_run_state;
+        }
+
+        self.ecs.maintain();
+    }
+}
+
+impl State {
+    fn run_systems(&mut self) {
+        systems::run(&self.ecs);
+        self.ecs.maintain();
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,85 +1,27 @@
 use bracket_geometry::prelude::Point;
-use bracket_lib::prelude::{main_loop, GameState};
+use bracket_lib::prelude::main_loop;
 use bracket_terminal;
-use bracket_terminal::prelude::{BError, BTerm, EMBED};
+use bracket_terminal::prelude::{BError, EMBED};
 
 use specs::prelude::*;
 
-pub mod components;
-pub mod map;
-pub mod message_log;
-pub mod types;
-pub mod ui;
-pub mod world;
-
-use components::register_components;
-use map::{Map, MAP_HEIGHT, MAP_WIDTH};
-use message_log::MessageLog;
-use types::{RunState, UITask};
-use ui::common::UIState; // FIXME: move to UI setup
-use ui::keyboard::{classic_laptop, Keybindings};
-use ui::UI;
-use world::engine::WorldEngineState;
-use world::spawner::{
+use reconnection_roguelike::components::register_components;
+use reconnection_roguelike::map::{Map, MAP_HEIGHT, MAP_WIDTH};
+use reconnection_roguelike::message_log::MessageLog;
+use reconnection_roguelike::types::RunState;
+use reconnection_roguelike::ui::common::UIState; // FIXME: move to UI setup
+use reconnection_roguelike::ui::keyboard::{classic_laptop, Keybindings};
+use reconnection_roguelike::ui::UI;
+use reconnection_roguelike::world::engine::WorldEngineState;
+use reconnection_roguelike::world::spawner::{
     create_bandage, create_enemy_big_stalker, create_enemy_hound, create_first_aid_kit,
     create_player,
 };
-use world::systems;
-use world::WorldEngine; // FIXME: move to WorldEngine setup
+use reconnection_roguelike::world::WorldEngine; // FIXME: move to WorldEngine setup
 
-pub const GAME_TITLE: &str = "Reconnection";
+use reconnection_roguelike::State;
 
 bracket_terminal::embedded_resource!(TILE_FONT, "../resources/reconnection_16x24_tiles_at_2x.png");
-
-pub struct State {
-    ecs: World,
-}
-
-// Implement the game loop
-impl GameState for State {
-    fn tick(&mut self, ctx: &mut BTerm) {
-        let ui = *self.ecs.fetch::<UI>();
-        let world_engine = *self.ecs.fetch::<WorldEngine>();
-
-        let mut new_run_state;
-        {
-            let run_state = self.ecs.fetch::<RunState>();
-            new_run_state = *run_state;
-        }
-
-        match new_run_state {
-            RunState::PreRun => {
-                self.run_systems();
-                new_run_state = RunState::DeferToUIFor(UITask::GetPlayerAction);
-            }
-            RunState::DeferToUIFor(ui_task) => match ui_task {
-                UITask::GetPlayerAction => {
-                    new_run_state = ui.defer_to_get_player_action(ctx, &mut self.ecs);
-                }
-                UITask::ShowWorldEvent => {
-                    new_run_state = ui.defer_to_show_world_event(ctx, &mut self.ecs);
-                }
-            },
-            RunState::WorldTick => {
-                new_run_state = world_engine.defer_to(&mut self.ecs);
-            }
-        }
-
-        {
-            let mut run_state_writer = self.ecs.write_resource::<RunState>();
-            *run_state_writer = new_run_state;
-        }
-
-        self.ecs.maintain();
-    }
-}
-
-impl State {
-    fn run_systems(&mut self) {
-        systems::run(&self.ecs);
-        self.ecs.maintain();
-    }
-}
 
 fn main() -> BError {
     bracket_terminal::link_resource!(TILE_FONT, "../resources/reconnection_16x24_tiles_at_2x.png");
