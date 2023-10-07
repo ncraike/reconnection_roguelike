13760a1cd712a1efc288235c93b7be27d401895c
Natasha Craike <ncraike@gmail.com>
2023-10-07 18:55:16 +1000

    ui:units:tiles: add new_width and new_height
    Add helper constructors Tiles1x::new_width() and Tiles1x::new_height().
    These create a Width and Height wrapper of Tiles1x from a given i32.

--- src/tests/units.rs
+++ src/tests/units.rs
@@ -2,6 +2,12 @@ use crate::ui::units::{Pixels, Tiles1x, Width};
 
 #[test]
 fn tile1x_width_in_pixels() {
-    let tiles_wide = Width::<Tiles1x>(Tiles1x(3));
+    let tiles_wide = Tiles1x::new_width(3);
     assert_eq!(Pixels::from(tiles_wide), Pixels(48));
+}
+
+#[test]
+fn tile1x_height_in_pixels() {
+    let tiles_high = Tiles1x::new_height(5);
+    assert_eq!(Pixels::from(tiles_high), Pixels(120));
 }
--- src/ui/units/tiles.rs
+++ src/ui/units/tiles.rs
@@ -6,6 +6,16 @@ pub struct Tiles1x(pub i32);
 pub type Tiles1xWidth = Width<Tiles1x>;
 pub type Tiles1xHeight = Height<Tiles1x>;
 
+impl Tiles1x {
+    pub fn new_width(value: i32) -> Width<Tiles1x> {
+        Width::<Tiles1x>(Tiles1x(value))
+    }
+
+    pub fn new_height(value: i32) -> Height<Tiles1x> {
+        Height::<Tiles1x>(Tiles1x(value))
+    }
+}
+
 impl From<Tiles1xWidth> for Pixels {
     fn from(width: Tiles1xWidth) -> Self {
         let quantity = width.0 .0;
