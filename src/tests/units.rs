ad8d8653fce7543e02ec7bdc17b206d7d9313d7f
Natasha Craike <ncraike@gmail.com>
2023-10-07 21:42:06 +1000

    ui::units::text: width/height helpers and tests

--- src/tests/units.rs
+++ src/tests/units.rs
@@ -1,4 +1,4 @@
-use crate::ui::units::{Pixels, Tiles1x, Tiles2x};
+use crate::ui::units::{Pixels, Text, Tiles1x, Tiles2x};
 
 #[test]
 fn tile1x_width_in_pixels() {
@@ -22,4 +22,16 @@ fn tile2x_width_in_pixels() {
 fn tile2x_height_in_pixels() {
     let tiles2x_high = Tiles2x::new_height(5);
     assert_eq!(Pixels::from(tiles2x_high), Pixels(240));
+}
+
+#[test]
+fn text_width_in_pixels() {
+    let text_width = Text::new_width(4);
+    assert_eq!(Pixels::from(text_width), Pixels(32));
+}
+
+#[test]
+fn text_height_in_pixels() {
+    let text_height = Text::new_height(3);
+    assert_eq!(Pixels::from(text_height), Pixels(48));
 }
--- src/ui/units/text.rs
+++ src/ui/units/text.rs
@@ -6,6 +6,16 @@ pub struct Text(pub i32);
 pub type TextWidth = Width<Text>;
 pub type TextHeight = Height<Text>;
 
+impl Text {
+    pub fn new_width(value: i32) -> Width<Text> {
+        Width::<Text>(Text(value))
+    }
+
+    pub fn new_height(value: i32) -> Height<Text> {
+        Height::<Text>(Text(value))
+    }
+}
+
 impl From<TextWidth> for Pixels {
     fn from(width: TextWidth) -> Self {
         let quantity = width.0 .0;
