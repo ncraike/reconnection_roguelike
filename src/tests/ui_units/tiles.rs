6dd5c369cd4a4b50cfc09994fedf17f8a266d533
Natasha Craike <ncraike@gmail.com>
2023-10-08 19:07:28 +1000

    Add/remove new helpers for UI tiles helpers
    Added new_point2d and new_size2d new helpers for UI units Tiles1x and
    Tiles2x.
    
    Removed new_width and new_height helpers, because I don't think I'll use
    them.

--- src/tests/ui_units/tiles.rs
+++ src/tests/ui_units/tiles.rs
@@ -1,4 +1,4 @@
-use crate::ui::units::{Pixels, Tiles1x, Tiles2x};
+use crate::ui::units::{Height, Pixels, Point2D, PosX, PosY, Size2D, Tiles1x, Tiles2x, Width};
 
 #[test]
 fn tiles1x_add() {
@@ -12,45 +12,83 @@ fn tiles1x_mul() {
 
 #[test]
 fn tiles1x_width_in_pixels() {
-    let tiles_wide = Tiles1x::new_width(3);
+    let tiles_wide = Width(Tiles1x(3));
     assert_eq!(Pixels::from(tiles_wide), Pixels(48));
 }
 
 #[test]
 fn tiles1x_height_in_pixels() {
-    let tiles_high = Tiles1x::new_height(5);
+    let tiles_high = Height(Tiles1x(5));
     assert_eq!(Pixels::from(tiles_high), Pixels(120));
 }
 
 #[test]
 fn tiles1x_width_add() {
-    assert_eq!(
-        Tiles1x::new_width(1) + Tiles1x::new_width(2),
-        Tiles1x::new_width(3)
-    );
+    assert_eq!(Width(Tiles1x(1)) + Width(Tiles1x(2)), Width(Tiles1x(3)));
 }
 
 #[test]
 fn tiles1x_width_mul() {
-    assert_eq!(Tiles1x::new_width(3) * 4, Tiles1x::new_width(12));
+    assert_eq!(Width(Tiles1x(3)) * 4, Width(Tiles1x(12)));
+}
+
+#[test]
+fn tiles1x_new_point2d() {
+    assert_eq!(
+        Tiles1x::new_point2d(3, 4),
+        Point2D {
+            x: PosX(Tiles1x(3)),
+            y: PosY(Tiles1x(4)),
+        }
+    );
+}
+
+#[test]
+fn tiles1x_new_size2d() {
+    assert_eq!(
+        Tiles1x::new_size2d(1, 2),
+        Size2D {
+            w: Width(Tiles1x(1)),
+            h: Height(Tiles1x(2)),
+        }
+    );
 }
 
 #[test]
 fn tiles2x_width_in_pixels() {
-    let tiles2x_wide = Tiles2x::new_width(3);
+    let tiles2x_wide = Width(Tiles2x((3)));
     assert_eq!(Pixels::from(tiles2x_wide), Pixels(96));
 }
 
 #[test]
 fn tiles2x_height_in_pixels() {
-    let tiles2x_high = Tiles2x::new_height(5);
+    let tiles2x_high = Height(Tiles2x(5));
     assert_eq!(Pixels::from(tiles2x_high), Pixels(240));
 }
 
 #[test]
 fn tiles2x_height_add() {
+    assert_eq!(Height(Tiles2x(1)) + Height(Tiles2x(2)), Height(Tiles2x(3)));
+}
+
+#[test]
+fn tiles2x_new_point2d() {
     assert_eq!(
-        Tiles1x::new_height(1) + Tiles1x::new_height(2),
-        Tiles1x::new_height(3)
+        Tiles2x::new_point2d(3, 4),
+        Point2D {
+            x: PosX(Tiles2x(3)),
+            y: PosY(Tiles2x(4)),
+        }
+    );
+}
+
+#[test]
+fn tiles2x_new_size2d() {
+    assert_eq!(
+        Tiles2x::new_size2d(1, 2),
+        Size2D {
+            w: Width(Tiles2x(1)),
+            h: Height(Tiles2x(2)),
+        }
     );
 }
--- src/ui/units/tiles.rs
+++ src/ui/units/tiles.rs
@@ -1,17 +1,25 @@
 extern crate derive_more;
 use super::base::{Height, Pixels, Width};
+use super::point_and_size::{Point2D, Size2D};
+use super::pos_x_y::{PosX, PosY};
 use derive_more::{Add, Mul};
 
 #[derive(Debug, Clone, Copy, PartialEq, Add, Mul)]
 pub struct Tiles1x(pub i32);
 
 impl Tiles1x {
-    pub fn new_width(value: i32) -> Width<Tiles1x> {
-        Width::<Tiles1x>(Tiles1x(value))
+    pub fn new_point2d(x: i32, y: i32) -> Point2D<Tiles1x> {
+        Point2D::<Tiles1x> {
+            x: PosX(Tiles1x(x)),
+            y: PosY(Tiles1x(y)),
+        }
     }
 
-    pub fn new_height(value: i32) -> Height<Tiles1x> {
-        Height::<Tiles1x>(Tiles1x(value))
+    pub fn new_size2d(w: i32, h: i32) -> Size2D<Tiles1x> {
+        Size2D::<Tiles1x> {
+            w: Width(Tiles1x(w)),
+            h: Height(Tiles1x(h)),
+        }
     }
 }
 
@@ -33,25 +41,31 @@ impl From<Height<Tiles1x>> for Pixels {
 pub struct Tiles2x(pub i32);
 
 impl Tiles2x {
-    pub fn new_width(value: i32) -> Width<Tiles2x> {
-        Width::<Tiles2x>(Tiles2x(value))
+    pub fn new_point2d(x: i32, y: i32) -> Point2D<Tiles2x> {
+        Point2D::<Tiles2x> {
+            x: PosX(Tiles2x(x)),
+            y: PosY(Tiles2x(y)),
+        }
     }
 
-    pub fn new_height(value: i32) -> Height<Tiles2x> {
-        Height::<Tiles2x>(Tiles2x(value))
+    pub fn new_size2d(w: i32, h: i32) -> Size2D<Tiles2x> {
+        Size2D::<Tiles2x> {
+            w: Width(Tiles2x(w)),
+            h: Height(Tiles2x(h)),
+        }
     }
 }
 
 impl From<Width<Tiles2x>> for Pixels {
     fn from(width: Width<Tiles2x>) -> Self {
         let quantity = width.0 .0;
-        Pixels::from(Tiles1x::new_width(quantity * 2))
+        Pixels::from(Width(Tiles1x(quantity * 2)))
     }
 }
 
 impl From<Height<Tiles2x>> for Pixels {
     fn from(height: Height<Tiles2x>) -> Self {
         let quantity = height.0 .0;
-        Pixels::from(Tiles1x::new_height(quantity * 2))
+        Pixels::from(Height(Tiles1x(quantity * 2)))
     }
 }
