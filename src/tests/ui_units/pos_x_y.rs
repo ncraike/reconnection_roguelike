d080191229257422f79454e0ab15527c4ad63cae
Natasha Craike <ncraike@gmail.com>
2023-10-11 14:54:33 +1000

    Implement Sub for PosX and PosY
    Add Sub implementations for PosX and PosY, returning Width and Height.

--- src/tests/ui_units/pos_x_y.rs
+++ src/tests/ui_units/pos_x_y.rs
@@ -6,6 +6,21 @@ fn posx_add_width() {
 }
 
 #[test]
+fn posx_ord() {
+    assert!(PosX(Pixels(7)) > PosX(Pixels(5)));
+}
+
+#[test]
+fn posx_sub_posx_gives_width() {
+    assert_eq!(PosX(Pixels(7)) - PosX(Pixels(3)), Width(Pixels(4)))
+}
+
+#[test]
 fn posy_add_height() {
     assert_eq!(PosY(Pixels(4)) + Height(Pixels(3)), PosY(Pixels(7)))
+}
+
+#[test]
+fn posy_ord() {
+    assert!(PosY(Pixels(5)) > PosY(Pixels(3)));
 }
