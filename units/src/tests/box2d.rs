722da4db97d023f096768e769e16a2dcf9b8694d
Natasha C <ncraike@gmail.com>
2024-02-24 17:30:10 +1000

    Units: test and fix Box2DI32 split_from_left
    Geometry is hard.

--- units/src/tests/box2d.rs
+++ units/src/tests/box2d.rs
@@ -1,5 +1,5 @@
 use super::base::MyUnitI32;
-use crate::{Box2DI32, HeightI32, PosXI32, PosYI32, Position2DI32, Size2DI32, UnitI32, WidthI32};
+use crate::{Box2DI32, HeightI32, PosXI32, PosYI32, Position2DI32, Size2DI32, WidthI32};
 
 #[test]
 fn x1_y1_x2_y2() {
@@ -107,4 +107,50 @@ fn normalize_picks_top_left_bottom_right_corners() {
         },
     };
     assert_eq!(box2d.normalize(), normal_box2d);
+}
+
+#[test]
+fn split_from_left() {
+    let orig_box = Box2DI32::<MyUnitI32> {
+        p1: Position2DI32 {
+            x: PosXI32(MyUnitI32(2)),
+            y: PosYI32(MyUnitI32(3)),
+        },
+        p2: Position2DI32 {
+            x: PosXI32(MyUnitI32(7)),
+            y: PosYI32(MyUnitI32(8)),
+        },
+    };
+    let (left_box, right_box) = orig_box.split_from_left(WidthI32(MyUnitI32(2)));
+    assert_eq!(left_box.p1, orig_box.p1);
+    assert_eq!(right_box.p2, orig_box.p2);
+    assert_eq!(left_box.width() + right_box.width(), orig_box.width());
+    assert_eq!(left_box.height(), orig_box.height());
+    assert_eq!(right_box.height(), orig_box.height());
+    assert_eq!(
+        left_box,
+        Box2DI32 {
+            p1: Position2DI32 {
+                x: PosXI32(MyUnitI32(2)),
+                y: PosYI32(MyUnitI32(3)),
+            },
+            p2: Position2DI32 {
+                x: PosXI32(MyUnitI32(4)),
+                y: PosYI32(MyUnitI32(8)),
+            },
+        }
+    );
+    assert_eq!(
+        right_box,
+        Box2DI32 {
+            p1: Position2DI32 {
+                x: PosXI32(MyUnitI32(4)),
+                y: PosYI32(MyUnitI32(3)),
+            },
+            p2: Position2DI32 {
+                x: PosXI32(MyUnitI32(7)),
+                y: PosYI32(MyUnitI32(8)),
+            },
+        }
+    );
 }
