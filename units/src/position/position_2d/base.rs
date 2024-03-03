23cbe91f6a0c57afc2c33816e8426a0ae4351a71
Natasha Craike <ncraike@gmail.com>
2024-03-03 23:26:00 +1000

    Units: tests for Position2D buffer index functions
    Added tests for `to_buffer_index()` and `from_buffer_index()`.
    
    Also changed `to_buffer_index()` to panic if Position2D's x is greater
    than the given width, as this almost certainly means a logic error
    somewhere else.
    
    Tests did not find any bugs, so current behaviour *seems* to be correct.

--- units/src/position/position_2d/base.rs
+++ units/src/position/position_2d/base.rs
@@ -40,6 +40,9 @@ impl<T: Unit> Position2D<T> {
         let x: usize = self.x.to_primitive().try_into().ok().unwrap();
         let y: usize = self.y.to_primitive().try_into().ok().unwrap();
         let w: usize = width.to_primitive().try_into().ok().unwrap();
+        if x > w {
+            panic!("Position2D's x {} is greater than the given width {}", x, w);
+        }
         (y * w) + x
     }
 
@@ -48,5 +51,91 @@ impl<T: Unit> Position2D<T> {
             x: PosX(T::new(index as i32 % width.to_primitive())),
             y: PosY(T::new(index as i32 / width.to_primitive())),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::Position2D;
+    use crate::example::MyUnit;
+    use crate::{PosX, PosY, Width};
+
+    #[test]
+    fn to_buffer_index_pos_0_0_gives_0() {
+        let pos2d = Position2D::<MyUnit> {
+            x: PosX(MyUnit(0)),
+            y: PosY(MyUnit(0)),
+        };
+        assert_eq!(pos2d.to_buffer_index(Width(MyUnit(10))), 0 as usize);
+    }
+
+    #[test]
+    fn to_buffer_index_x_of_0_gives_x_if_less_than_width() {
+        let pos2d = Position2D::<MyUnit> {
+            x: PosX(MyUnit(9)),
+            y: PosY(MyUnit(0)),
+        };
+        assert_eq!(pos2d.to_buffer_index(Width(MyUnit(10))), 9 as usize);
+    }
+
+    #[test]
+    fn to_buffer_index_y_of_1_gives_width() {
+        let pos2d = Position2D::<MyUnit> {
+            x: PosX(MyUnit(0)),
+            y: PosY(MyUnit(1)),
+        };
+        assert_eq!(pos2d.to_buffer_index(Width(MyUnit(7))), 7 as usize);
+    }
+
+    #[test]
+    #[should_panic]
+    fn to_buffer_index_should_panic_if_x_greater_than_width() {
+        let pos2d = Position2D::<MyUnit> {
+            x: PosX(MyUnit(20)),
+            y: PosY(MyUnit(3)),
+        };
+        pos2d.to_buffer_index(Width(MyUnit(10)));
+    }
+
+    #[test]
+    fn to_buffer_index_y_of_4_times_width_of_5_plus_x_of_3_equals_23() {
+        let pos2d = Position2D::<MyUnit> {
+            x: PosX(MyUnit(3)),
+            y: PosY(MyUnit(4)),
+        };
+        assert_eq!(pos2d.to_buffer_index(Width(MyUnit(5))), 23 as usize);
+    }
+
+    #[test]
+    fn from_buffer_index_with_index_0_gives_pos_0_0() {
+        assert_eq!(
+            Position2D::from_buffer_index(0, Width(MyUnit(10))),
+            Position2D {
+                x: PosX(MyUnit(0)),
+                y: PosY(MyUnit(0)),
+            }
+        );
+    }
+
+    #[test]
+    fn from_buffer_index_with_index_eq_width_gives_pos_0_1() {
+        assert_eq!(
+            Position2D::from_buffer_index(10, Width(MyUnit(10))),
+            Position2D {
+                x: PosX(MyUnit(0)),
+                y: PosY(MyUnit(1)),
+            }
+        );
+    }
+
+    #[test]
+    fn from_buffer_index_with_index_17_width_7_gives_pos_3_2() {
+        assert_eq!(
+            Position2D::from_buffer_index(17, Width(MyUnit(7))),
+            Position2D {
+                x: PosX(MyUnit(3)),
+                y: PosY(MyUnit(2)),
+            }
+        );
     }
 }
